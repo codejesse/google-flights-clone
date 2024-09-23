03507f47b7e3ad1233acae4ed07ca00a2743d97b
Jesse <jessebeke10@gmail.com>
2024-09-23 17:09:35 +0100

    feat: setting up navbar and drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import "./App.css";
+import hero from "../src/assets/hero.svg";
+
 
 function App() {
   return (
-    <>
+    <div>
+      <img src={hero} alt="hero-image" />
       <h1>Hello niggas</h1>
-    </>
+    </div>
   );
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,15 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { Menu } from "@mui/icons-material";
+
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex bg-white py-4 drop-shadow-sm text-bold">
+      <div className="mx-8">
+        <Menu />
+      </div>
+    </nav>
+  );
 };
